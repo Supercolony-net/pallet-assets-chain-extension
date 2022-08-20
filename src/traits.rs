011096a5435c9b46368134dc58ac1dacb7309600
xgreenx <xgreenx9999@gmail.com>
2022-08-20 22:45:46 +0100

    Set explicit extension id.

--- src/traits.rs
+++ src/traits.rs
@@ -67,7 +67,8 @@ pub trait Environment {
 }
 
 // TODO: Add comments
-#[obce::definition]
+/// id = 2 bytes of blake2b("pallet-assets-chain-extension@v0.1")
+#[obce::definition(id = 0x48f6)]
 pub trait PalletAssets<T: Environment> {
     fn create(&mut self, id: T::AssetId, admin: T::AccountId, min_balance: T::Balance) -> Result<(), Error<T>>;
 
